68ee3db0b7b588ebfcac198d7c1553ba3a0bf070
Cindy Lehner <clehner@student.tgm.ac.at>
2015-11-30 09:22:33 +0100

    Himmelskörper
    Himmelskörper Klasse befüllt

--- Himmelskoerper.py
+++ Himmelskoerper.py
@@ -1,1 +1,26 @@
-__author__ = 'Cindy Lehner'
+__author__ = 'cindylehner, lukaszainzinger'
+
+import pygame
+from OpenGL.raw.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+class Himmelskoerper():
+
+    def __init__(self):
+        self.size = None
+        self.rotate = None
+        self.orbitB = None
+
+    def create(self):
+        pass
+
+    def performRotate(self):
+        pass
+
+
+    def performOrbit(self):
+        pass
+
+    def getTexture(self):
+        pass
