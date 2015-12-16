26e438d3b9cd74924ad8e0c83eb5c8beb221fbaa
Lukas Zainzinger <lzainzinger@student.tgm.ac.at>
2015-12-16 14:59:48 +0100

    Erde kreist um Sonne 1
    Die Erde kreist um die Sonne, ohne Mond
    Beides sind WireSphere s mit einer Farbe

--- Sunsystem.py
+++ Sunsystem.py
@@ -17,14 +17,21 @@ from OpenGL.GLU import *
 #    self.hourOfDay = 0.0
 #   self.dayOfYear = 0.0
 #  self.marsDayOfYear = 0.0
-
 # Methode zum Erstellen von Planeten
-def sphere():
-    glColor3f(0.0, 0.0, 1.0)
-    glutWireSphere(0.25, 30, 30)
+def sphereSonne():
+    glColor3f(1.0, 1.0, 0.0)
+    glutWireSphere(1.0, 50, 50)
+
+def sphereErde():
+    glColor3f(0.2, 0.2, 1.0)
+    glutWireSphere(0.35, 40, 40)
+
 
 
 def main():
+    dayOfYear = 0.0
+    hourOfDay = 0.0
+    animateIncrement = 4.0
     #  """ Game - Main """
 
     # Frame
@@ -33,11 +40,23 @@ def main():
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     pygame.display.set_caption('Sunsystem - Lehner, Zainzinger')
 
-    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glShadeModel(GL_SMOOTH)
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glEnable(GL_DEPTH_TEST)
 
-    glTranslatef(0.0, 0.0, -5.0)
+    glMatrixMode(GL_PROJECTION)
 
-    glRotatef(0, 0, 0, 0)
+    gluPerspective(60, (display[0] / display[1]), 0.5, 100.0)
+
+    glTranslatef(0.0, -0.5, -2.0)
+
+    glRotatef(1, 1, 1, 1)
+
+    glMatrixMode(GL_MODELVIEW)
+
+
+    paused = False
 
     # Game-Loop
     while True:
@@ -49,19 +68,37 @@ def main():
                 pygame.quit()
                 quit()
 
-        # Rotieren des Planeten
-        glRotatef(1, 0, 1, 0)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        if not paused:
+            # Update the animation state
+            hourOfDay += animateIncrement
+            inc = animateIncrement / 24.0
+            dayOfYear += inc
+            hourOfDay -= (hourOfDay // 24 * 24)
+            dayOfYear -= (dayOfYear // 365 * 365)
 
-        # Um den Planeten zu drehen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslatef(0.0, 0.0, -8.0)
+        glRotatef(15.0, 1.0, 0.0, 0.0)
+
+        # Sonne
         glPushMatrix()
         glRotatef(-90, 1.0, .0, .0)
-        sphere()
+        sphereSonne()
         glPopMatrix()
 
+        # Erde
+        glRotatef(360.0 * dayOfYear / 365.0, 0.0, 1.0, 0.0)
+        glTranslatef(4.0, 0.0, 0.0)
+        glPushMatrix()
+        glRotatef(360.0 * hourOfDay / 24.0, 0.0, 1.0, 0.0)
+        sphereErde()
+        glPopMatrix()
+
+        glFlush()
         # Frame update
+        pygame.time.wait(40)
         pygame.display.flip()
-        pygame.time.wait(10)
 
     pygame.quit()
     quit()
